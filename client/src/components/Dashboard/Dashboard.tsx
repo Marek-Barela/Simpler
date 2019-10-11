863adc58b4095477bfabe77b9f76bdd8c63eb410
Marek Bareła <marek.barela@wp.pl>
2019-10-11 19:02:31 +0200

    basic dashboard structure commited

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from "react";
+import logoIcon from "../../assets/images/simpler-icon.png";
 import styles from "./Dashboard.module.css";
 
 const Dashboard: FC = () => {
-  const { container } = styles;
+  const {
+    container,
+    navBar,
+    navbarWrapper,
+    iconContainer,
+    navigationConntainer,
+    contentWrapper,
+    aside,
+    content
+  } = styles;
   return (
     <div className={container}>
-      <h1>Welcome in Dashboard</h1>
+      <nav className={navBar}>
+        <div className={navbarWrapper}>
+          <div className={iconContainer}>
+            <img src={logoIcon} alt="" />
+          </div>
+          <div className={navigationConntainer}>icon</div>
+        </div>
+      </nav>
+      <main className={contentWrapper}>
+        <aside className={aside}>sidebar</aside>
+        <section className={content}>content</section>
+      </main>
     </div>
   );
 };
