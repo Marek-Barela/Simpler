52234a9ff5b7fd58b7212a0c74d9ef2320c624b1
Marek Bareła <marek.barela@wp.pl>
2019-10-12 20:55:05 +0200

    delete and update project api commited

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -55,7 +55,65 @@ router.get("/:id", auth, async (req, res) => {
   catch (err) {
     console.error(err.message);
     if(err.kind === "ObjectId") {
-      return res.status(404).json({ msg: "Note not found" })
+      return res.status(404).json({ msg: "Project not found" })
+    }
+    res.status(500).send("Server error");
+  }
+})
+
+// @route     PUT api/projects/:id
+// @desc      Edit project title
+// @access    Private
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+
+    if(project.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized"});
+    }
+
+    if(!project) {
+      return res.status(404).json({ msg: "Project not found" })
+    }
+
+    project.title = req.body.title;
+
+    await project.save()
+
+    res.json({ msg: "project updated" });
+  }
+  catch (err) {
+    console.error(err.message);
+    if(err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Project not found" })
+    }
+    res.status(500).send("Server error");
+  }
+})
+
+// @route     DELETE api/projects/:id
+// @desc      Delete projects by id
+// @access    Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const project = await Project.findByIdAndUpdate(req.params.id);
+
+    if(project.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized"});
+    }
+
+    if(!project) {
+      return res.status(404).json({ msg: "Project not found" })
+    }
+
+    await project.remove();
+
+    res.json({ msg: "project deleted"});
+  }
+  catch (err) {
+    console.error(err.message);
+    if(err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Project not found" })
     }
     res.status(500).send("Server error");
   }
