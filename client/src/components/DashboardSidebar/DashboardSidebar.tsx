683a0f330443ebf252cb092657e990fcb379c68c
Marek Bareła <marek.barela@wp.pl>
2019-10-13 19:02:08 +0200

    basic sidebard dropdown commited

--- client/src/components/DashboardSidebar/DashboardSidebar.tsx
+++ client/src/components/DashboardSidebar/DashboardSidebar.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import FontAwesomeIcon from "../FontAwesomeIcon";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import styles from "./DashboardSidebar.module.css";
 
 const DashboardSidebar = () => {
-  const { aside } = styles;
-  return <aside className={aside}>sidebar</aside>;
+
+  const [dropdownIsVisible, setDropdownIsVisible] = useState(true);
+
+  const switchDropdownVisiblity = () => {
+    setDropdownIsVisible(!dropdownIsVisible)
+  }
+
+  const { 
+    aside, 
+    projectsDropdown, 
+    dropdownButton, 
+    dropdownButtonCaret,
+    dropdownButtonCaretRotate,
+    dropdownList,
+    dropdownListHidden,
+    dropdownItem
+  } = styles;
+  const caretStyling = dropdownButtonCaret + ` ${dropdownIsVisible && dropdownButtonCaretRotate}`;
+  const dropdownListStyles = dropdownList + ` ${!dropdownIsVisible && dropdownListHidden}`;
+  return (
+    <aside className={aside}>
+      <div className={projectsDropdown}>
+        <button 
+          className={dropdownButton} 
+          onClick={() => switchDropdownVisiblity()}
+        >
+          <span className={caretStyling}>
+            <FontAwesomeIcon icon={faAngleRight}/>
+          </span>
+          Projects
+        </button>
+        <ul className={dropdownListStyles}>
+          <li className={dropdownItem}>item</li>
+        </ul>
+      </div>
+    </aside>
+  );
 };
 
 export default DashboardSidebar;
