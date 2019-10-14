e35a785f806f370495fa4cf323f76006248443eb
Marek Bareła <marek.barela@wp.pl>
2019-10-14 21:09:45 +0200

    delete button in dropdown list item commited

--- client/src/components/DashboardSidebarDropdownListItem/DashboardSidebarDropdownListItem.tsx
+++ client/src/components/DashboardSidebarDropdownListItem/DashboardSidebarDropdownListItem.tsx
@@ -1,15 +1,30 @@
 import React, { FC } from "react";
+import FontAwesomeIcon from "../FontAwesomeIcon";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { ProjectsResponse } from "../Dashboard/Dashboard-model";
 import styles from "./DashboardSidebarDropdownListItem.module.css";
 
 type Props = ProjectsResponse;
 
 const DashboardSidebarDropdownListItem: FC<Props> = ({ color, title }) => {
-  const { dropdownItem, dropdownItemBubble } = styles;
+  const {
+    dropdownItem,
+    itemWrapper,
+    dropdownItemBubble,
+    dropdownItemButton
+  } = styles;
   return (
     <li className={dropdownItem}>
-      <span className={dropdownItemBubble} style={{ backgroundColor: color }} />
-      {title}
+      <div className={itemWrapper}>
+        <span
+          className={dropdownItemBubble}
+          style={{ backgroundColor: color }}
+        />
+        {title}
+      </div>
+      <button className={dropdownItemButton}>
+        <FontAwesomeIcon icon={faTrashAlt} />
+      </button>
     </li>
   );
 };
