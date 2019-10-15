f18a90f823842a5a634bc067fd5dae6f8aa8650b
Marek Bareła <marek.barela@wp.pl>
2019-10-15 21:40:49 +0200

    task item sstyling commited

--- client/src/components/DashboardContentTasksItem/DashboardContentTasksItem.tsx
+++ client/src/components/DashboardContentTasksItem/DashboardContentTasksItem.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from "react";
+import FontAwesomeIcon from "../FontAwesomeIcon";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { TasksResponse } from "../Dashboard/Dashboard-model";
 import styles from "./DashboardContentTasksItem.module.css";
 
 type Props = TasksResponse;
 
 const DashboardContentTasksItem: FC<Props> = ({ description }) => {
-  const { taskItem } = styles;
-  return <li className={taskItem}>{description}</li>;
+  const handleTaskDoubleClick = () => {
+    console.log("work");
+  };
+
+  const { taskItem, taskWrapper, taskButton, editButton } = styles;
+  return (
+    <li className={taskItem} onDoubleClick={handleTaskDoubleClick}>
+      <div className={taskWrapper}>
+        <button className={taskButton}></button>
+        {description}
+      </div>
+      <button className={editButton}>
+        <FontAwesomeIcon icon={faEdit} />
+      </button>
+    </li>
+  );
 };
 
 export default DashboardContentTasksItem;
