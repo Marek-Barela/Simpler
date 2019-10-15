cb98a33c5c09f7b603ca5ca4b7e7725ee9dacaab
Marek Bareła <marek.barela@wp.pl>
2019-10-15 22:18:54 +0200

    task item disabled styling commited

--- client/src/components/DashboardContentTasksItem/DashboardContentTasksItem.tsx
+++ client/src/components/DashboardContentTasksItem/DashboardContentTasksItem.tsx
@@ -1,24 +1,47 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import FontAwesomeIcon from "../FontAwesomeIcon";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { TasksResponse } from "../Dashboard/Dashboard-model";
 import styles from "./DashboardContentTasksItem.module.css";
 
 type Props = TasksResponse;
 
 const DashboardContentTasksItem: FC<Props> = ({ description }) => {
+  const [taskIsFinishing, setTaskIsFinishing] = useState(false);
+
   const handleTaskDoubleClick = () => {
+    if (taskIsFinishing) return;
     console.log("work");
   };
 
-  const { taskItem, taskWrapper, taskButton, editButton } = styles;
+  const handleFinishTaskButton = () => {
+    setTaskIsFinishing(true);
+  };
+
+  const handleEditButtonClick = () => {
+    if (taskIsFinishing) return;
+    console.log("work");
+  };
+
+  const {
+    taskItem,
+    taskWrapper,
+    taskButton,
+    editButton,
+    taskActivated
+  } = styles;
+
+  const taskItemStyling = taskItem + ` ${taskIsFinishing && taskActivated}`;
+
   return (
-    <li className={taskItem} onDoubleClick={handleTaskDoubleClick}>
+    <li className={taskItemStyling} onDoubleClick={handleTaskDoubleClick}>
       <div className={taskWrapper}>
-        <button className={taskButton}></button>
+        <button className={taskButton} onClick={handleFinishTaskButton}>
+          <FontAwesomeIcon icon={faCheck} />
+        </button>
         {description}
       </div>
-      <button className={editButton}>
+      <button className={editButton} onClick={handleEditButtonClick}>
         <FontAwesomeIcon icon={faEdit} />
       </button>
     </li>
